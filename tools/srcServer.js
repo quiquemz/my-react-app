dc887030ec91684465c38887c68e84dc0d4fa08d
quiquemz <semendez@ucsd.edu>
2017-03-28 23:59:47 -0600

    PLEASE WORK PROD 1

--- tools/srcServer.js
+++ tools/srcServer.js
@@ -13,17 +13,17 @@ var app = express();
 var compiler = webpack(config);
 
 if (process.env.NODE_ENV !== 'production') {
-    app.use(require('webpack-dev-middleware')(compiler, {
-        noInfo: true,
-        publicPath: config.output.publicPath
-    }));
-
     app.use(require('webpack-hot-middleware')(compiler));
-
-    app.get('*', function (req, res) {
-        res.sendFile(path.join(__dirname, '../src/index.html'));
-    });
 }
+
+app.use(require('webpack-dev-middleware')(compiler, {
+    noInfo: true,
+    publicPath: config.output.publicPath
+}));
+
+app.get('*', function (req, res) {
+    res.sendFile(path.join(__dirname, '../src/index.html'));
+});
 
 app.listen(port, function (err) {
     if (err) {
